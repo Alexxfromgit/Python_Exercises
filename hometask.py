ef4037abeefa525e5f30341c6773c5696db864e4
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-11-21 11:38:31 +0200

    Update hometask.py
    add target 1

--- hometask.py
+++ hometask.py
@@ -6,4 +6,5 @@ ages = [12, 33, 56, 87, 4, 18, 21]
 text_a = "Special cases aren't special enough to break the rules."
 text_b = "In the face of ambiguity, refuse the temptation to guess."
 
+print(len(names))
 
