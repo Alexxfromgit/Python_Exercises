6c56af479ab62ad16d5a1e3b23de56442f8d4ad1
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-11-27 17:37:46 +0200

    Output index for each element in the list

--- hometask2.py
+++ hometask2.py
@@ -7,3 +7,8 @@ names = ['Ann', 'Fillip', 'Monika', 'Ross', 'Sam', 'Alf', 'Roy', 'Sasha', 'Leon'
 for i in range(len(names))
     print(names[i])
 
+# Task2
+for i in range(len(names))
+    print(names.index(names[i]))
+
+
