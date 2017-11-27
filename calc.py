fb90c0ba718ae9a9d856c6a2e3387b9536b5be3d
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-11-28 00:34:41 +0200

    Add addition_all

--- calc.py
+++ calc.py
@@ -1,20 +1,30 @@
 #Creating a file
 
 #Summ function
+
 def summ(a, b):
     return a + b
 
 #Subtraction function
+
 def sub(a, b):
     return a - b
 
 #Multiplication function
+
 def mul(a, b):
     return a * b
 
 #Division function
+
 def div(a, b):
     return a / b
 
+#Addition_all
 
+def addition_all(*args):
+    x = 0
+    for i in args:
+        x += i
+    return x
 
