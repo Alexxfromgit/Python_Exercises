bea8e8a5481406dc5fa3cc7c4aea5061c467749e
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-11-28 16:27:08 +0200

    Reorganized the function's logic
    Still needs more powerful machine to count this

--- Even_Fibonacci_numbers.py
+++ Even_Fibonacci_numbers.py
@@ -12,13 +12,10 @@ find the sum of the even-valued terms.
 def fib(n):
 
     f = [0, 1]
+    count = 0
 
     for i in range(2, n + 1):
         f.append(f[i-1] + f[i-2])
-
-    count = 0
-
-    for i in range(len(f)):
         if f[i] % 2 == 0:
             count += f[i]
 
