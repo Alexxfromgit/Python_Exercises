3f4a41351078f8fc49bcf2db0b27d422d0ebf9a1
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-11-30 02:49:48 +0200

    Add Jedi task

--- seasons.py
+++ seasons.py
@@ -59,4 +59,53 @@ if __name__ == '__main__':
     assert season(10) == 'autumn'
     assert season(11) == 'autumn'
      
+def season(month):
+    '''
+        Задача для Джидаев:
+        Написать функцию, вводишь название месяца или номер месяца (т.е. число int()), возвращает время года:
+    '''
+    if month in [1, 2, 12] or month in ['January', 'February', 'December']:
+        return 'winter'
+    elif month in [3, 4, 5] or month in ['March', 'April', 'May']:
+        return 'spring'
+    elif month in [6, 7, 8] or month in ['June', 'July', 'August']:
+        return 'summer'
+    elif month in [9, 10, 11] or month in ['September', 'October', 'November']:
+        return 'autumn'
+    else:
+        return 'Something is wrong'
+
+
+if __name__ == '__main__':
+    assert season(1) == 'winter'
+    assert season(2) == 'winter'
+    assert season(12) == 'winter'
+
+    assert season(3) == 'spring'
+    assert season(4) == 'spring'
+    assert season(5) == 'spring'
+
+    assert season(6) == 'summer'
+    assert season(7) == 'summer'
+    assert season(8) == 'summer'
+
+    assert season(9) == 'autumn'
+    assert season(10) == 'autumn'
+    assert season(11) == 'autumn'
+
+    assert season('January') == 'winter'
+    assert season('February') == 'winter'
+    assert season('December') == 'winter'
+
+    assert season('March') == 'spring'
+    assert season('April') == 'spring'
+    assert season('May') == 'spring'
+
+    assert season('June') == 'summer'
+    assert season('July') == 'summer'
+    assert season('August') == 'summer'
+
+    assert season('September') == 'autumn'
+    assert season('October') == 'autumn'
+    assert season('November') == 'autumn'
      
