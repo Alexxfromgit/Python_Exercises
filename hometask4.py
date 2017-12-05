5dbf6ece7a05ff875097bb51a2fffb96b554fa83
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-12-06 01:54:12 +0200

    Task2 completed

--- hometask4.py
+++ hometask4.py
@@ -25,3 +25,35 @@ class Boxer:
 
 vasiliy = Boxer(22, 100, 150)
 
+#Task2
+
+class Person:
+    '''
+    1. All of [body.age, body.first_name, body.second_name, body.birthday_month, 
+    body.full_name(), body.email(), body.birth_season()] works correct.
+    '''
+    def __init__(self, age, first_name, second_name, birthday_month):
+        self.age = age
+        self.first_name = first_name
+        self.second_name = second_name
+        self.birthday_month = birthday_month
+
+    def full_name(self):
+        return self.first_name + " " + self.second_name
+
+    def email(self):
+        return self.first_name + "." + self.second_name + "@email.com"
+
+    def birth_season(self):
+        if self.birthday_month in ['January', 'February', 'December']:
+            return 'Winter'
+        if self.birthday_month in ['March', 'April', 'May']:
+            return 'Winter'
+        if self.birthday_month in ['June', 'July', 'August']:
+            return 'Winter'
+        if self.birthday_month in ['September', 'October', 'November']:
+            return 'Winter'
+
+
+body = Person(age='18', first_name='Pedro', second_name='Villalobos', birthday_month='March')
+
