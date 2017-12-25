423d5c13776c2d3481874f55325d613c1343964c
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-12-25 19:34:17 +0200

    Add changing values in statements

---
+++ decorators_python.py
@@ -0,0 +1,19 @@
+# Create file Decorators Python
+
+# Task 1
+  """
+  1. Есть переменные 
+  a = 5
+  b = 0
+  поменять значения переменных, не используя, новцю переменную
+  """
+  
+  a = 5
+  b = 0
+  
+  a = - a - b
+  b = - a - b
+  a = - a - b
+  
+  # Result will be changed values in statements
+  
