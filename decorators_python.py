955f7b8b1de3c05b2123155fe76a0b3d7cbf2b98
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-12-25 20:49:54 +0200

    Add food's decorators

--- decorators_python.py
+++ decorators_python.py
@@ -17,3 +17,58 @@
   
   # Result will be changed values in statements
   
+# Task 2
+  """
+  Запилить кодец, чтоб делал сендвич
+  """
+  
+def bread(food):
+    """Bread decorator"""
+    def bread_func():
+        print("</'''''''\>")
+        food()
+        print("<\_______/>")
+    return bread_func
+
+
+def cotlet(food):
+    """Cotlet decorator"""
+    def cotlet_func():
+        print("==cotlet==")
+        food()
+        print("==cotlet==")
+    return cotlet_func
+
+
+def salad(food):
+    """Salad decorator"""
+    def salad_func():
+        food()
+        print("~~~salad~~~")
+    return salad_func
+
+
+def tomato(food):
+    """Tomato decorator"""
+    def tomato_func():
+        print("_tomato_")
+        food()
+    return tomato_func
+
+
+def cheese(food):
+    """Cheese decorator"""
+    def cheese_func():
+        print("[[[cheese]]]")
+        food()
+        print("[[[cheese]]]")
+    return cheese_func
+
+
+@bread
+@cheese
+@cotlet
+@salad
+@tomato
+def sandwich():
+    print("--ham--")
